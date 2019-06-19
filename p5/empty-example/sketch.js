7323489767bb355b200a29edcc2cdf2ccf3f9bee
横田 南進 <qcs6429@quick.local>
2019-06-19 11:09:15 +0900

    6.2 Classes in JavaScript with ES6

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,42 @@
+let bubble1;
+let bubble2;
+
+function setup() {
+  createCanvas(600, 400);
+  bubble1 = new Bubble();
+  bubble2 = new Bubble();
+}
+
+function draw() {
+  background(0);
+  bubble1.move();
+  bubble1.show();
+  bubble2.move();
+  bubble2.show();
+}
+
+//constructor | instanceを作成(new class)した時点で実行されるメソッド(関数)のこと
+class Bubble {
+  constructor() {
+    this.x = 200;
+    this.y = 150;
+  }
+
+  move() {
+    this.x = this.x + random(-5,5);
+    this.y = this.y + random(-5,5);
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, 24, 24);
+  }
+}
+
+// function show() {
+// }
+
+// function move() {
+// }
