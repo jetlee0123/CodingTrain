e766eb379638b13e66e0bcc2ef3cdc6f41bd5139
横田 南進 <qcs6429@quick.local>
2019-06-19 14:37:09 +0900

    7.4:Mouse Interation with Objects

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -2,13 +2,28 @@ let bubbles = [];
 
 function setup() {
   createCanvas(600, 400);
+  for (let i = 0; i < 5; i++) {
+    let x = random(width);
+    let y = random(height);
+    let r = random(10, 50);
+    let b = new Bubble(x, y, r);
+    bubbles.push(b)
+  }
+
+
 }
 
-function mouseDragged() {
-  let r = random(10, 50);
-  let b = new Bubble(mouseX, mouseY, r);
-  bubbles.push(b)
+function mousePressed() {
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].clicked(mouseX,mouseY);
+  }
 }
+
+// function mousePressed() {
+//   let r = random(10, 50);
+//   let b = new Bubble(mouseX, mouseY, r);
+//   bubbles.push(b)
+// }
 
 function draw() {
   background(0);
@@ -20,12 +35,21 @@ function draw() {
 
 //class=設計図
 //constructor=instanceを作成(new class)した時点で実行されるメソッド(関数)のこと
-//constructorの引数には、new class_nameの引数が入る
+
 class Bubble {
   constructor(x, y, r) {
     this.x = x;
     this.y = y;
     this.r = r;
+    this.brightness = 0;
+  }
+
+  clicked(px, py) {
+    let d = dist(px, py, this.x, this.y); //dist(x1,y1,x2,y2)
+    if (d < this.r) {
+      this.brightness = 255;
+      // console.log("CLICKED ON BUBBLE!");
+    }
   }
 
   move() {
@@ -36,7 +60,7 @@ class Bubble {
   show() {
     stroke(255);
     strokeWeight(4);
-    noFill();
+    fill(this.brightness, 100);
     ellipse(this.x, this.y, this.r * 2);
   }
 }
