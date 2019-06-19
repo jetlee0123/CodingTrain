a4b076c63bf49015dc045bf2b1fa905afa4b2241
横田 南進 <qcs6429@quick.local>
2019-06-19 11:46:14 +0900

    1. What is an array?(number and string)

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,45 +1,32 @@
-let bubble1;
-let bubble2;
+/*
+1. What is an array?(number and string)
+2. Make an array of objects(literal objects)
+3. Constructor function to make objects
+4. push() and splice()
+5. Objects that you interact with (clicking on an object)
+6. Objects that talk to each other (nested loop)
+*/
+
+var words = ["rainbow","heart","purple","friendship","love"];
+
+var index = 0;
 
 function setup() {
-  createCanvas(600, 400);
-  bubble1 = new Bubble(200, 200, 40); //constructorの引数(width,height,radius)
-  bubble2 = new Bubble(400, 200, 20);
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(0);
-  bubble1.move();
-  bubble1.show();
-  bubble2.move();
-  bubble2.show();
+
+  fill(255);
+  textSize(32);
+  text(words[index],12,200);
 }
 
-//class=設計図
-//constructor=instanceを作成(new class)した時点で実行されるメソッド(関数)のこと
-//constructorの引数には、new class_nameの引数が入る
-class Bubble {
-  constructor(unicorn, fluffy, rainbow) { /
-    this.x = unicorn;
-    this.y = fluffy;
-    this.r = rainbow;
-  }
+function mousePressed() {
+  index = index + 1;
 
-  move() {
-    this.x = this.x + random(-5, 5);
-    this.y = this.y + random(-5, 5);
-  }
-
-  show() {
-    stroke(255);
-    strokeWeight(4);
-    noFill();
-    ellipse(this.x, this.y, this.r*2);
+  if (index == words.length) {
+    index = 0;
   }
 }
-
-// function show() {
-// }
-
-// function move() {
-// }
