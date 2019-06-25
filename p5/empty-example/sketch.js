dee846db5376f3f7a5f90b89a7cd76bbfe9399de
横田 南進 <qcs6429@quick.local>
2019-06-25 19:05:27 +0900

    （途中）ゲーム化

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,41 +1,52 @@
-//成果物1)ゲーム
+//成果物1)ピンボールゲーム
 //bubbleをn個出して全部消すまでのタイムを競う
-//タイムに応じてランクを決める
+//タイムに応じてランクが決める
+
+// bubblesを個別にバラバラに動かす
+// 追加：カウントダウン機能を搭載
+// 追加：残り時間によってランクを分ける
+// 追加：bubblesを画像に置き換え
+// 追加：マウスカーソルを他の画像にする
+// 【済】追加：bubblesが空になったらメッセージ
 
 let bubbles = [];
+let speedX = 5;
+let speedY = 3;
 
 function setup() {
   createCanvas(600, 400);
-
+  for (let i = 0; i < random(10); i++){
+    let x = random(width);
+    let y = random(height);
+    let r = random(20,30);
+    let b = new Bubble(x, y, r);
+    bubbles.push(b);
+  }
 }
 
-//bubbleを消す
-function mouseDragged() {
-  let r = 40;
-  let b = new Bubble(mouseX, mouseY, r);
-  bubbles.push(b);
-
+function mousePressed() {
+  for (let i = 0; i < bubbles.length; i++){
+    if (bubbles[i].contains(mouseX,mouseY)) {
+      bubbles.splice(i,1)
+      console.log("length",bubbles.length)
+      if (bubbles.length == 0){
+        alert('Clear!')
+      }
+    }
+  }
 }
 
 function draw() {
   background(0);
   for (let i = 0; i < bubbles.length; i++) {
-    if (bubbles[i].contains(mouseX, mouseY)){
-      bubbles[i].changeColor(255);
-    } else {
-      bubbles[i].changeColor(0);
+    if (bubbles[i].contains(mouseX,mouseY)){
+      bubbles[i].changeColor();
     }
     bubbles[i].move();
     bubbles[i].show();
   }
-
-  if (bubbles.length > 10) {
-    bubbles.splice(0,1);
-  }
 }
 
-//class=設計図
-//constructor=instanceを作成(new class)した時点で実行されるメソッド(関数)のこと
 class Bubble {
   constructor(x, y, r) {
     this.x = x;
@@ -44,22 +55,29 @@ class Bubble {
     this.brightness = 0;
   }
 
-  changeColor(bright) {
-    this.brightness = bright;
+  changeColor() {
+    this.brightness = 255;
   }
 
   contains(px, py) {
     let d = dist(px, py, this.x, this.y);
     if (d < this.r) {
-      return true; 
+      return true;
     } else {
       return false;
     }
   }
 
   move() {
-    this.x = this.x + random(-2, 2);
-    this.y = this.y + random(-2, 2);
+    if (this.x > width || this.y > height){
+      speedX = random(-5);
+      speedY = random(-3);
+    } else if (this.x < 0 || this.y < 0) {
+      speedX = random(5);
+      speedY = random(3);
+    }
+    this.x = this.x + speedX;
+    this.y = this.y + speedY;
   }
 
   show() {
